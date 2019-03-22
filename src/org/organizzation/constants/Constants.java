f493692bc3e0455f11ecc4142eaae65a868ee0ca
Ariel Esteban Solis <xlin01@informatica.aci.it>
2019-03-22 17:42:26 +0100

    Eliminazione delle costanti superflue

--- src/org/organizzation/constants/Constants.java
+++ src/org/organizzation/constants/Constants.java
@@ -8,8 +8,4 @@ public final class Constants  {
 	public static final BigDecimal DUTY_TAX = new BigDecimal(5);
 	public static final BigDecimal HUNDRED = new BigDecimal(100);
 	
-	public static final String BOOKS = "books";
-	public static final String FOOD = "food";
-	public static final String MEDICAL = "medical";
-	public static final String GENERICS = "generics";
 }
